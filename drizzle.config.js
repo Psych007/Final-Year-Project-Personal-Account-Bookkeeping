c51260e85b404deedd39254c2570d967d9d13784
Psych007 <130185819+Psych007@users.noreply.github.com>
2024-07-29 16:14:47 +0500

    Add files via upload

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+
+export default {
+    schema: "./utils/schema.jsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: "postgresql://neondb_owner:TGF6Iv4UOaCN@ep-lively-cloud-a5eak0dr.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require",
+    }
+  };
